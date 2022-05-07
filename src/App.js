9bed0c44560afaf3595777713ba937305e7945e9
MichaelR <spider747@gmail.com>
2022-05-07 17:37:12 +0300

    Add TimeRecord component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import TimeRecord from './components/TimeRecord';
+import dummyRecords from './dummyRecords';
 import './App.css';
+
+console.log(dummyRecords[0])
 
 function App() {
   return (
     <div className="App">
+      {dummyRecords.map((item) => {
+        return (
+          <TimeRecord key={item.id} {...item}/>
+        );
+      })}
+      
     </div>
   );
 }
---
+++ src/dummyRecords.js
@@ -0,0 +1,25 @@
+const dummyRecords = [
+    {
+        id: 1,
+        date: "7/5/2022",
+        activity: "Youtube watch",
+        hours: 3,
+        mode: "Wasted"
+    },
+    {
+        id: 2,
+        date: "7/5/2022",
+        activity: "Running",
+        hours: 1,
+        mode: "Productive"
+    },
+    {
+        id: 3,
+        date: "7/5/2022",
+        activity: "Learn",
+        hours: 2,
+        mode: "Productive"
+    },
+];
+
+export default dummyRecords;
