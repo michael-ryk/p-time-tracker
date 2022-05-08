80d7b7b6d843d11b57f233f127b4ceac6a6638b5
MichaelR <spider747@gmail.com>
2022-05-08 20:54:23 +0300

    Modify for mobile friendly

--- src/components/ShowRecords/TimeRecords.jsx
+++ src/components/ShowRecords/TimeRecords.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import TimeRecordItem from './TimeRecordItem';
+import './TimeRecords.css';
 
 function TimeRecord(props){
   return (
-    <>
+    <div className="timeRec">
       {props.records.map((item) => {
         return <TimeRecordItem key={item.id} {...item} />;
       })}
-    </>
+    </div>
   )
 }
 
