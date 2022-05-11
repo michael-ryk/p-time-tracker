e71965c82ca3612c780ae5d3e5f3de98b68f2169
MichaelR <spider747@gmail.com>
2022-05-11 20:39:28 +0300

    Add expand collapse for new record

--- src/components/NewItem/RecordForm.jsx
+++ src/components/NewItem/RecordForm.jsx
@@ -21,52 +21,71 @@ function RecordForm(props) {
     props.onSubmitRecord(newRecord);
   }
 
+  const [expandForm, setExpandForm] = useState(false)
+
+  function onExpand() {
+    setExpandForm(true);
+  }
+
+  function onCollapse() {
+    setExpandForm(false);
+  }
+
   return (
     <form onSubmit={submitHandler}>
       <div className='record-form__items'>
-        <div className='record-form__item'>
-          <label>Date:</label>
-          <input
-            name='date'
-            value={newRecord.date}
-            type='date'
-            onChange={onChange}
-            min='2020-01-01'
-          />
-        </div>
-        <div className='record-form__item'>
-          <label>Activity:</label>
-          <input
-            name='activity'
-            value={newRecord.activity}
-            type='text'
-            onChange={onChange}
-          />
-        </div>
-        <div className='record-form__item'>
-          <label>Hours:</label>
-          <input
-            name='hours'
-            value={newRecord.hours}
-            type='number'
-            onChange={onChange}
-            min='0'
-            step='0.1'
-          />
-        </div>
-        <div className='record-form__item'>
-          <label>Mode:</label>
-          <select name='mode' value={newRecord.mode} onChange={onChange}>
-            <option value='Productive'>Productive</option>
-            <option value='Wasted'>Wasted</option>
-          </select>
-        </div>
-        <div className='record-form__action'>
-          <button type='submit'>Add Record</button>
-        </div>
+        {expandForm === false ? (
+          <div className='record-form__action'>
+              <button onClick={onExpand}>Expand</button>
+          </div> 
+        ) : ( 
+          <>
+            <div className='record-form__item'>
+              <label>Date:</label>
+              <input
+                name='date'
+                value={newRecord.date}
+                type='date'
+                onChange={onChange}
+                min='2020-01-01'
+              />
+            </div>
+            <div className='record-form__item'>
+              <label>Activity:</label>
+              <input
+                name='activity'
+                value={newRecord.activity}
+                type='text'
+                onChange={onChange}
+              />
+            </div>
+            <div className='record-form__item'>
+              <label>Hours:</label>
+              <input
+                name='hours'
+                value={newRecord.hours}
+                type='number'
+                onChange={onChange}
+                min='0'
+                step='0.1'
+              />
+            </div>
+            <div className='record-form__item'>
+              <label>Mode:</label>
+              <select name='mode' value={newRecord.mode} onChange={onChange}>
+                <option value='Productive'>Productive</option>
+                <option value='Wasted'>Wasted</option>
+              </select>
+            </div>
+            <div className='record-form__action'>
+              <button type='submit'>Add Record</button>
+              <button onClick={onCollapse}>Cancel</button>
+            </div> 
+          </>
+          )}
       </div>
     </form>
-  );
+);
 }
 
 export default RecordForm;
