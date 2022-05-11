6e7e33a8818200bbf265a962ae841ad13cbe8787
MichaelR <spider747@gmail.com>
2022-05-11 20:44:21 +0300

    Set default filter state all

--- src/components/ShowRecords/TimeRecords.jsx
+++ src/components/ShowRecords/TimeRecords.jsx
@@ -5,12 +5,12 @@ import './TimeRecords.css';
 
 function TimeRecord(props){
 
-  const [filter, setFilter] = useState('Productive');
+  const [filter, setFilter] = useState('All');
 
   function filterChangeHandler(selectedType) {
     setFilter(selectedType);
   }
-
+  
   const filteredRecords = filter==="All" ? props.records : props.records.filter(item => item.mode===filter);
 
   return (
